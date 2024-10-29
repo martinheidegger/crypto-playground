cf4a5a9a2295aef52982c374bda433238b90d234
Martin Heidegger <martin.heidegger@gmail.com>
2024-10-30 02:19:09 +0900

    fix: publicPath property

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "https:/crypto-playground/",
+  publicPath: "/crypto-playground/",
   configureWebpack: {
     resolve: {
       fallback: {
