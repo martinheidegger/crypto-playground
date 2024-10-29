5d17b0b2fc5c4000c7e0fb99f73205ca0e0fbb47
Martin Heidegger <martin.heidegger@gmail.com>
2024-10-30 02:10:29 +0900

    setting base to crypto-playground

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "https:/crypto-playground/",
   configureWebpack: {
     resolve: {
       fallback: {
