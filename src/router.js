a5efd72859141be5b8f90ed1f2c4a596ed4a36a1
Martin Heidegger <martin.heidegger@gmail.com>
2024-10-30 01:26:34 +0900

    chore: removing unused customer pages

--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Customers from './views/Customers.vue'
-import CustomerDetails from './views/CustomerDetails.vue'
 import Crypto from './views/Crypto.vue'
 
 Vue.use(Router)
@@ -11,16 +9,6 @@ const router =  new Router({
     {
       path: '/',
       redirect: '/crypto'
-    },
-    {
-      path: '/customers',
-      name: 'customers',
-      component: Customers
-    },
-    {
-      path: '/customerdetails/:id',
-      name: 'customerdetails',
-      component: CustomerDetails
     },
     {
       path: '/crypto',
