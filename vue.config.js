0cc25bac64ed68a4e0158896658759d226fd7f23
Martin Heidegger <martin.heidegger@gmail.com>
2024-10-30 08:39:59 +0900

    fix: dependency

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "/crypto-playground/",
   configureWebpack: {
     resolve: {
       fallback: {
+        buffer: require.resolve('buffer/'),
         stream: require.resolve("stream-browserify")
       }
-    }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+          Buffer: ['buffer', 'Buffer'],
+      }),
+    ],
   }
 })
